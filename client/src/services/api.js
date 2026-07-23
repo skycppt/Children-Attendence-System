266e32ad6405231114b86ca5c3b07d23bab528d5
skycppt <sujit.arcade.jgec@gmail.com>
2026-07-23 14:55:38 +0530

    backend update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://attendance-backend-d64z.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://attendance-backend-d64z.onrender.com/api",
 });
 
 export default api;
