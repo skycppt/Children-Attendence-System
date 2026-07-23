a197201108f12e0616d46ac264698e7dea0e952a
skycppt <sujit.arcade.jgec@gmail.com>
2026-07-23 16:20:37 +0530

    report section visible

--- client/src/pages/Attendance.jsx
+++ client/src/pages/Attendance.jsx
@@ -201,7 +201,7 @@ function Attendance() {
 
       )}
 
-      {/* <div className="card shadow-sm mt-4">
+      <div className="card shadow-sm mt-4">
 
         <div className="card-body text-center">
 
@@ -218,7 +218,7 @@ function Attendance() {
 
         </div>
 
-      </div> */}
+      </div>
 
     </div>
   );
